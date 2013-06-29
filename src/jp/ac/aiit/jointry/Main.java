d9de833f40c9c61d94f0d87f4466b60507cbdbe9
kdmsnr <kdmsnr@gmail.com>
2013-06-29 21:33:48 +0900

    Modify fxml path

--- src/jp/ac/aiit/jointry/Main.java
+++ src/jp/ac/aiit/jointry/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("main/fxml/Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("controllers/fxml/Main.fxml"));
 
         Scene scene = new Scene(root);
 
