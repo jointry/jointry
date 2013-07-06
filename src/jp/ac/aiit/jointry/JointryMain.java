6de21ad09bf837c95fa7270c288371d3fe9edb50
kdmsnr <kdmsnr@gmail.com>
2013-07-06 22:04:23 +0900

    Refactoring: move code between controllers

--- src/jp/ac/aiit/jointry/JointryMain.java
+++ src/jp/ac/aiit/jointry/JointryMain.java
@@ -4,49 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import jp.ac.aiit.jointry.controllers.BackStageController;
-import jp.ac.aiit.jointry.controllers.BlocksController;
-import jp.ac.aiit.jointry.controllers.FrontStageController;
-import jp.ac.aiit.jointry.controllers.MainController;
 
 public class JointryMain extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader;
-
-        // Main
         loader = new FXMLLoader(getClass().getResource("controllers/fxml/Main.fxml"));
-        BorderPane parent = (BorderPane) loader.load();
-        MainController controller = loader.<MainController>getController();
+        Parent parent = (Parent) loader.load();
 
-        // FrontStage
-        loader = new FXMLLoader(getClass().getResource("controllers/fxml/FrontStage.fxml"));
-        Parent front = (Parent) loader.load();
-        FrontStageController fsc = loader.<FrontStageController>getController();
-        fsc.setMainController(controller);
-        parent.setRight(front);
-        controller.setFrontStageController(fsc);
-
-        // BackStage
-        loader = new FXMLLoader(getClass().getResource("controllers/fxml/BackStage.fxml"));
-        Parent back = (Parent) loader.load();
-        BackStageController bsc = loader.<BackStageController>getController();
-        fsc.setMainController(controller);
-        parent.setCenter(back);
-        controller.setBackStageController(bsc);
-
-        // BackStage
-        loader = new FXMLLoader(getClass().getResource("controllers/fxml/Blocks.fxml"));
-        Parent blocks = (Parent) loader.load();
-        BlocksController bc = loader.<BlocksController>getController();
-        bc.setMainController(controller);
-        parent.setLeft(blocks);
-        controller.setBlocksController(bc);
-
-        // Staget setting
         Scene scene = new Scene(parent);
         stage.setScene(scene);
         stage.show();
--- src/jp/ac/aiit/jointry/controllers/MainController.java
+++ src/jp/ac/aiit/jointry/controllers/MainController.java
@@ -1,17 +1,57 @@
 package jp.ac.aiit.jointry.controllers;
 
+import com.sun.javafx.css.BorderPaint;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
 
 public class MainController implements Initializable {
 
+    @FXML
+    private BorderPane pane;
     private BackStageController backStageController;
     private FrontStageController frontStageController;
     private BlocksController blocksController;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        try {
+            FXMLLoader ld;
+            Class<? extends MainController> klass = getClass();
+
+            // FrontStage
+            ld = new FXMLLoader(klass.getResource("fxml/FrontStage.fxml"));
+            Parent front = (Parent) ld.load();
+            FrontStageController fsc = ld.<FrontStageController>getController();
+            fsc.setMainController(this);
+            pane.setRight(front);
+            setFrontStageController(fsc);
+
+            // BackStage
+            ld = new FXMLLoader(klass.getResource("fxml/BackStage.fxml"));
+            Parent back = (Parent) ld.load();
+            BackStageController bsc = ld.<BackStageController>getController();
+            fsc.setMainController(this);
+            pane.setCenter(back);
+            setBackStageController(bsc);
+
+            // BackStage
+            ld = new FXMLLoader(klass.getResource("fxml/Blocks.fxml"));
+            Parent blocks = (Parent) ld.load();
+            BlocksController bc = ld.<BlocksController>getController();
+            bc.setMainController(this);
+            pane.setLeft(blocks);
+            setBlocksController(bc);
+        } catch (IOException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void setBackStageController(BackStageController controller) {
