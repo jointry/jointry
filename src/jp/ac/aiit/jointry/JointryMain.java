ba0c7bc861a0add61a4bfe2280c6ae4fa547efe0
kdmsnr <kdmsnr@gmail.com>
2013-07-06 18:07:57 +0900

    Fix import statements

--- src/jp/ac/aiit/jointry/JointryMain.java
+++ src/jp/ac/aiit/jointry/JointryMain.java
@@ -1,8 +1,5 @@
 package jp.ac.aiit.jointry;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
