cd446a9db0efc9cb81b348c045252dba9f5f3bf4
kdmsnr <kdmsnr@gmail.com>
2013-07-06 14:39:10 +0900

    Tweak

---
+++ src/jp/ac/aiit/jointry/controllers/BlocksController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.ac.aiit.jointry.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.shape.Rectangle;
+
+public class BlocksController implements Initializable {
+
+    @FXML
+    private Rectangle round;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+}
