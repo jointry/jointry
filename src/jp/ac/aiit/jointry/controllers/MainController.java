5e64be43ba04f85c022f2b63cd44e3ba56429b8e
kdmsnr <kdmsnr@gmail.com>
2013-10-02 21:08:09 +0900

    Dummy impl of fopen

--- src/jp/ac/aiit/jointry/controllers/MainController.java
+++ src/jp/ac/aiit/jointry/controllers/MainController.java
@@ -1,25 +1,34 @@
 package jp.ac.aiit.jointry.controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import jp.ac.aiit.jointry.models.Sprite;
 
 public class MainController implements Initializable {
 
     @FXML
     private BorderPane rootPane;
+    @FXML
+    private MenuItem fopen;
     private BackStageController backStageController;
     private FrontStageController frontStageController;
     private BlocksController blocksController;
+    @FXML
+    private Label dummylabel;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -85,5 +94,15 @@ public class MainController implements Initializable {
 
     public BlocksController getBlocksController() {
         return this.blocksController;
+    }
+
+    @FXML
+    public void fopen(ActionEvent event) {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Pick a Sound File");
+        File f = fc.showOpenDialog(dummylabel.getScene().getWindow());
+        if (f != null) {
+            // do something
+        }
     }
 }
