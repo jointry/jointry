82468b79c56eeae0a284b83c011143692bb407b4
kdmsnr <kdmsnr@gmail.com>
2013-12-07 18:26:09 +0900

    Fix Status class into Container class

--- src/jp/ac/aiit/jointry/models/Status.java
+++ src/jp/ac/aiit/jointry/models/Status.java
@@ -1,6 +1,23 @@
 package jp.ac.aiit.jointry.models;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
-public class Status extends HashMap<Object, Object> {
+public class Status extends HashMap {
+
+    private LinkedHashMap params;
+
+    /**
+     * @return the params
+     */
+    public LinkedHashMap getParams() {
+        return params;
+    }
+
+    /**
+     * @param params the params to set
+     */
+    public void setParams(LinkedHashMap params) {
+        this.params = params;
+    }
 }
