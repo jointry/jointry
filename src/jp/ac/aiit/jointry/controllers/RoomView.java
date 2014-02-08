74dfee70bb779fe014188c5bd0c3aba4705abfee
MK <a1215mk@aiit.ac.jp>
2014-02-08 16:07:18 +0900

    Change room view

--- src/jp/ac/aiit/jointry/controllers/RoomView.java
+++ src/jp/ac/aiit/jointry/controllers/RoomView.java
@@ -17,7 +17,6 @@ import jp.ac.aiit.jointry.models.Room;
 public class RoomView extends AnchorPane {
 
     private Label userName;
-    private Label id;
     private Room room;
 
     public RoomView(int roomId, Room room) {
@@ -28,33 +27,24 @@ public class RoomView extends AnchorPane {
 
         userName = new Label(room.getName());
         userName.setAlignment(Pos.CENTER);
-        userName.setLayoutX(79.0);
+        userName.setLayoutX(5.0);
         userName.setLayoutY(2.0);
-        userName.prefHeight(38.0);
+        userName.prefWidth(38.0);
         userName.prefHeight(22.0);
         userName.setFont(new Font(36.0));
         userName.setTextFill(Color.WHITE);
-
-        id = new Label("" + roomId);
-        id.setLayoutX(8.0);
-        id.setLayoutY(4.0);
-        id.setPrefWidth(37.0);
-        id.setPrefHeight(31.0);
-        id.setFont(new Font(36.0));
-        id.setTextFill(Color.WHITE);
 
         ImageView imageview = new ImageView(getClass().getResource("images/room.png").toString());
         imageview.setFitWidth(258.0);
         imageview.setFitHeight(38.0);
         imageview.setLayoutX(1.0);
         imageview.setLayoutY(1.0);
-        this.getChildren().addAll(imageview, id, userName);
+        this.getChildren().addAll(imageview, userName);
     }
 
     public void setRoom(int roomId, Room room) {
         this.room = room;
         this.userName.setText(room.getName());
-        this.id.setText("" + roomId);
     }
 
     public Room getRoom() {
